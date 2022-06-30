48486426106a2c1f388cfb61b27553a3fb571948
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-06-30 15:45:48 -0400

    write connection.js, index.js, schema.sql, seeds.sql

---
+++ db/index.js
@@ -0,0 +1,60 @@
+const connection = require('./connection');
+
+class DB {
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+    findAllEmployees() {
+        return this.connection.promise().query(`SELECT * FROM employee`);
+    }
+
+    createEmployee(employee) {
+        return this.connection.promise().query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                                                VALUES (?, ?, ?, ?)`);
+    }
+
+    updateEmployeeRole() {
+    return this.connection.promise().query(`UPDATE employee SET role_id = ?
+                                            WHERE id = ?`);
+    }
+
+    findAllRoles() {
+    return this.connection.promise().query(`SELECT * FROM role`);
+    }
+
+    findAllDepartments(department) {
+        return this.connection.promise().query(`SELECT * FROM department`);
+
+    }
+
+    createDepartment() {
+        return this.connection.promise().query(`INSERT INTO department (department_name)
+                                                VALUES (?)`);
+
+    }
+
+    createRole() {
+        return this.connection.promise().query(`INSERT INTO role (title, salary, department_id)
+                                                VALUES (?, ?, ?)`);
+
+    }
+
+}
+
+
+// removeEmployee(employeeId) {
+//     return this.connection.promise().query(SQL)
+// }
+// viewBudjet()
+
+// viewEmployeesByDepartment()
+
+// viewEmployeesByManager()
+
+// updateEmployeeManager()
+
+// removeDepartment()
+
+// removeRole()
+module.exports = new DB(connection);
---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS company;
+CREATE DATABASE company;
+USE company;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INTEGER
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
+);
+
+-- constraints?
